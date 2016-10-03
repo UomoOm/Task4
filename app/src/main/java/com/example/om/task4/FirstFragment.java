c354bb5beb959b81a0039b472af118de9eb98104
Om <uomoom@gmail.com>
2016-10-03 17:26:27 +0300

    master

---
+++ app/src/main/java/com/example/om/task4/FirstFragment.java
@@ -0,0 +1,23 @@
+package com.example.om.task4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+    public static FirstFragment newInstance(String param1, String param2) {
+
+        return fragment_first;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/om/task4/ThirdFragment.java
@@ -0,0 +1,23 @@
+package com.example.om.task4;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThirdFragment extends Fragment {
+
+    public static ThirdFragment newInstance(String param1, String param2) {
+
+        return fragment_third;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+}
